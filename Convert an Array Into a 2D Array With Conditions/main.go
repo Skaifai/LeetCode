685ef1926759500755bb1bfc3598af07abe98654
Arman <real.alzhanov@gmail.com>
2024-01-03 01:02:55 +0600

    Update solution
    Add other solutions to the "Convert an Array Into a 2D Array With Conditions" problem

--- Convert an Array Into a 2D Array With Conditions/main.go
+++ Convert an Array Into a 2D Array With Conditions/main.go
@@ -6,6 +6,21 @@ func main() {
 	fmt.Println(findMatrix([]int{1, 3, 4, 1, 2, 3, 1}))
 }
 
+func findMatrix(nums []int) [][]int {
+	var result [][]int
+	integers := make(map[int]int)
+
+	for _, num := range nums {
+		if len(result) == integers[num] {
+			result = append(result, []int{})
+		}
+		result[integers[num]] = append(result[integers[num]], num)
+		integers[num]++
+	}
+
+	return result
+}
+
 //func findMatrix(nums []int) [][]int {
 //	var result [][]int
 //	integers := make(map[int]int)
@@ -18,7 +33,7 @@ func main() {
 //			result[i] = append(result[i], integer)
 //		}
 //
-//		for len(result) <= integers[integer] {
+//		for len(result) < integers[integer] {
 //			result = append(result, []int{integer})
 //		}
 //	}
@@ -26,25 +41,25 @@ func main() {
 //	return result
 //}
 
-func findMatrix(nums []int) [][]int {
-	var result [][]int
-	count := 0
-	integers := make(map[int]int)
-	for i := 0; i < len(nums); i++ {
-		integers[nums[i]]++
-		count++
-	}
-
-	for count > 0 {
-		intermediate := make([]int, 0)
-		for integer := range integers {
-			if integers[integer] > 0 {
-				count--
-				intermediate = append(intermediate, integer)
-				integers[integer]--
-			}
-		}
-		result = append(result, intermediate)
-	}
-	return result
-}
+//func findMatrix(nums []int) [][]int {
+//	var result [][]int
+//	count := 0
+//	integers := make(map[int]int)
+//	for i := 0; i < len(nums); i++ {
+//		integers[nums[i]]++
+//		count++
+//	}
+//
+//	for count > 0 {
+//		intermediate := make([]int, 0)
+//		for integer := range integers {
+//			if integers[integer] > 0 {
+//				count--
+//				intermediate = append(intermediate, integer)
+//				integers[integer]--
+//			}
+//		}
+//		result = append(result, intermediate)
+//	}
+//	return result
+//}
