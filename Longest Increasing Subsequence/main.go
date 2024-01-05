6d1b522c5c361798deefc4c3d1df59eb88b15f04
Arman <real.alzhanov@gmail.com>
2024-01-06 03:24:29 +0600

    Add solution
    Add a solution to the "Longest Increasing Subsequence" problem using Binary Search

--- Longest Increasing Subsequence/main.go
+++ Longest Increasing Subsequence/main.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	fmt.Println(lengthOfLIS([]int{0, 1, 0, 3, 2, 3}))
+	fmt.Println(lengthOfLIS([]int{0, 1, 0, 3, 4, 2, 3, 4, 5}))
 }
 
 func max(a int, b int) int {
@@ -13,36 +16,53 @@ func max(a int, b int) int {
 	return b
 }
 
-// DP (memoization) + Recursion
+// Binary Search
 
 func lengthOfLIS(nums []int) int {
-	result := 1
+	sub := []int{nums[0]}
 
-	dp := make([]int, len(nums))
-
-	for i := 0; i < len(nums); i++ {
-		if dp[i] == 0 {
-			dp[i] = recursion(i, nums, dp)
-		}
-		result = max(result, dp[i])
-	}
-
-	return result
-}
-
-func recursion(index int, nums []int, dp []int) int {
-	if dp[index] != 0 {
-		return dp[index]
-	}
-	res := 0
-	for i := index + 1; i < len(nums); i++ {
-		if nums[i] > nums[index] {
-			res = max(res, recursion(i, nums, dp))
+	for i := 1; i < len(nums); i++ {
+		num := nums[i]
+		if num > sub[len(sub)-1] {
+			sub = append(sub, num)
+		} else {
+			j := sort.Search(len(sub), func(m int) bool { return sub[m] >= num })
+			sub[j] = num
 		}
 	}
-	dp[index] = res + 1
-	return dp[index]
+	return len(sub)
 }
+
+// DP (memoization) + Recursion
+
+//func lengthOfLIS(nums []int) int {
+//	result := 1
+//
+//	dp := make([]int, len(nums))
+//
+//	for i := 0; i < len(nums); i++ {
+//		if dp[i] == 0 {
+//			dp[i] = recursion(i, nums, dp)
+//		}
+//		result = max(result, dp[i])
+//	}
+//
+//	return result
+//}
+//
+//func recursion(index int, nums []int, dp []int) int {
+//	if dp[index] != 0 {
+//		return dp[index]
+//	}
+//	res := 0
+//	for i := index + 1; i < len(nums); i++ {
+//		if nums[i] > nums[index] {
+//			res = max(res, recursion(i, nums, dp))
+//		}
+//	}
+//	dp[index] = res + 1
+//	return dp[index]
+//}
 
 // Recursion with no DP
 
