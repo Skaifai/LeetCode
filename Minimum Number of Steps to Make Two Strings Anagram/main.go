1bdf967ddcbc2997461fb23675018d6a306ac62d
Arman <real.alzhanov@gmail.com>
2024-01-13 20:34:56 +0600

    Update solution
    Add another solution to the "Minimum Number of Steps to Make Two Strings Anagran" problem

--- Minimum Number of Steps to Make Two Strings Anagram/main.go
+++ Minimum Number of Steps to Make Two Strings Anagram/main.go
@@ -6,26 +6,43 @@ func main() {
 	fmt.Println(minSteps("abb", "aab"))
 }
 
-func minSteps(s string, t string) int {
-	res := 0
-
-	var word [26]int
-
-	for i := 0; i < len(s); i++ {
-		word[s[i]-'a']++
-		word[t[i]-'a']--
-	}
-
-	for i := 0; i < 26; i++ {
-		res += abs(word[i])
-	}
-
-	return res / 2
-}
-
 func abs(n int) int {
 	if n < 0 {
 		return -n
 	}
 	return n
 }
+
+func minSteps(s string, t string) int {
+	res := 0
+
+	word := map[uint8]int{}
+
+	for i := 0; i < len(s); i++ {
+		word[s[i]-'a']++
+		word[t[i]-'a']--
+	}
+
+	for _, i := range word {
+		res += abs(i)
+	}
+
+	return res / 2
+}
+
+//func minSteps(s string, t string) int {
+//	res := 0
+//
+//	var word [26]int
+//
+//	for i := 0; i < len(s); i++ {
+//		word[s[i]-'a']++
+//		word[t[i]-'a']--
+//	}
+//
+//	for i := 0; i < 26; i++ {
+//		res += abs(word[i])
+//	}
+//
+//	return res / 2
+//}
